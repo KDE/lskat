a800683f01849a5aacb68e49a03e6c77f75bf2e8
Martin Heni <martin@heni-online.de>
2000-06-01 20:01:29 +0000

    Lieutnant sKat: A two player KDE card game. Runs stable.
    svn path=/trunk/kdegames/lskat/; revision=51532

---
+++ config.h
@@ -0,0 +1,25 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "lskat"
+
+/* Version number of package */
+#define VERSION "0.9"
+
+/* Define if NLS is requested */
+#define ENABLE_NLS 1
+
+/* Define if you have libz */
+#define HAVE_LIBZ 1
+
+/* Define if you have libpng */
+#define HAVE_LIBPNG 1
+
+/* Defines if your system has the crypt function */
+#define HAVE_CRYPT 1
+
+/* Define the type of the third argument for getsockname
+ */
+#define ksize_t socklen_t
+
