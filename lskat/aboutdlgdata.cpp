5959c10f18a2a79d5036cb1fcc7bffa7764680bd
Hans Petter Bieker <bieker@kde.org>
2000-06-17 10:43:00 +0000

    Ok -> OK.
    svn path=/trunk/kdegames/lskat/; revision=53476

--- lskat/aboutdlgdata.cpp
+++ lskat/aboutdlgdata.cpp
@@ -75,7 +75,7 @@ void  aboutDlg::initDialog(){
   QPushButton_1->setGeometry(110,250,100,30);
   QPushButton_1->setMinimumSize(0,0);
   QPushButton_1->setFocus();
-  QPushButton_1->setText(i18n("Ok"));
+  QPushButton_1->setText(i18n("OK"));
 
 }
 
