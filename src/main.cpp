59c578195f50077689abe8388c0f75fcebd9521b
Martin Heni <martin@heni-online.de>
2007-04-24 04:51:46 +0000

    Changed comment
    svn path=/trunk/KDE/kdegames/lskat/; revision=657442

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@
 
 static KCmdLineOptions options[] =
 {
-    { "d", 0, 0},
+  { "d", 0, 0},
   { "debug <level>", I18N_NOOP("Enter debug level"), 0 },
   { "skipintro", I18N_NOOP("Skip intro animation"), 0 },
   { "demo", I18N_NOOP("Run game in demo (autoplay) mode"), 0 },
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
                         LSKAT_VERSION,
                         I18N_NOOP("Lskat: A desktop card game"),
                         KAboutData::License_GPL,
-                        "(c) 1995-2006, Martin Heni");
+                        "(c) 1995-2007, Martin Heni");
   aboutData.addAuthor("Martin Heni",0, "kde@heni-online.de");
   aboutData.addCredit("KDE", I18N_NOOP("KDE"), 0);
   aboutData.addAuthor("Benjamin Meyer", I18N_NOOP("Code Improvements"), 0);
