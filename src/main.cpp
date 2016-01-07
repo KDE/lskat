ec363ba9cdec6b90d0a133deb58c9da52dcf9159
Frederik Schwarzer <schwarzer@kde.org>
2016-01-07 17:46:52 +0100

    Fix build.

--- src/main.cpp
+++ src/main.cpp
@@ -125,7 +125,7 @@ int main(int argc, char *argv[])
     mainwindow->show();
   }
 
-  app.setWindowIcon(QIcon::fromTheme(QStringLiteral("lskat")));
+  application.setWindowIcon(QIcon::fromTheme(QStringLiteral("lskat")));
 
   return application.exec();
 }
